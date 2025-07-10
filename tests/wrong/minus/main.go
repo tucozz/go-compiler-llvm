cf3a935984db997e24df70ddf9ab4ce4b069740e
PhenBD <pedrinho_phbd@hotmail.com>
2025-07-10 16:03:52 -0300

    testes com erros para verificação de tipos

---
+++ tests/wrong/minus/main.go
@@ -0,0 +1,18 @@
+func main() {
+    a = 5 - 5;
+    b = 5 - 5.0;
+    c = 5 - true;
+    d = 5 - "Hello";
+    e = 5.0 - 5;
+    f = 5.0 - 5.0;
+    g = 5.0 - true;
+    h = 5.0 - "Hello";
+    i = true - 5;
+    j = true - 5.0;
+    k = true - true;
+    l = true - "Hello";
+    m = "Hello" - 5;
+    n = "Hello" - 5.0;
+    o = "Hello" - true;
+    p = "Hello" - "World";
+}
